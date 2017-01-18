61d47371a75633c2d15e3e1eaaaa9b57d40ddd6b
ayonmkhan <mohtasimtamjeed@gmail.com>
2017-01-18 18:22:41 +0600

    git init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $("ul.nav-tabs.stats-tabs li").click(function(e) {
+  $("ul.nav-tabs.tabs li").click(function(e) {
 
     e.preventDefault();
 
     var tabId = $(this).attr("data-tab");
 
-    $('ul.nav-tabs.stats-tabs li').removeClass("active");
-    $('ul.nav-tabs.stats-tabs ~ [id^=tab]').addClass("hidden");
+    $('ul.nav-tabs.tabs li').removeClass("active");
+    $('ul.nav-tabs.tabs ~ [id^=tab]').addClass("hidden");
 
     $(this).addClass("active");
     $("#"+tabId).removeClass("hidden");
