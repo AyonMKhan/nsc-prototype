2e7c9cd2104c126b11f8094acb5c7c4bffd41fab
AyonMKhan <mohtasimtamjeed@gmail.com>
2017-01-31 00:49:52 +0600

    megamenu and other changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,59 @@
-$(document).ready(function() {
-  $("ul.nav-tabs.tabs li").click(function(e) {
+(function () {
+    "use strict";
 
-    e.preventDefault();
+    $(document).ready(function () {
+        $("ul.nav-tabs.tabs li").click(function (e) {
 
-    var tabId = $(this).attr("data-tab");
+            e.preventDefault();
 
-    $('ul.nav-tabs.tabs li').removeClass("active");
-    $('ul.nav-tabs.tabs ~ [id^=tab]').addClass("hidden");
+            var tabId = $(this).attr("data-tab");
 
-    $(this).addClass("active");
-    $("#"+tabId).removeClass("hidden");
+            $('ul.nav-tabs.tabs li').removeClass("active");
+            $('ul.nav-tabs.tabs ~ [id^=tab]').addClass("hidden");
+            
+            $(this).addClass("active");
+            $("#" + tabId).removeClass("hidden");
 
-  })
-
-
-  $("p.see-more-toggle").click(function() {
-    //e.preventDefault();//prevent default link clicking behavior
-
-    if ($(this).text() === "See more...") {
-      
-      $("p.see-more-toggle").parent().children(".see-more").removeClass("hidden");
-      $(this).text("See less...");
-    } 
-    
-    else {
-      $(this).text("See more...");
-      $("p.see-more-toggle").parent().children(".see-more").addClass("hidden");
-    }
-  });
+        });
 
 
-  $("div.gallery").click(function() {
-    $("#gallery-modal").modal();
-  });
+        $("p.see-more-toggle").click(function () {
+            //e.preventDefault();//prevent default link clicking behavior
+
+            if ($(this).text() === "See more...") {
+
+                $("p.see-more-toggle").parent().children(".see-more").removeClass("hidden");
+                $(this).text("See less...");
+            } else {
+                $(this).text("See more...");
+                $("p.see-more-toggle").parent().children(".see-more").addClass("hidden");
+            }
+        });
 
 
-//jquery for left menu
-/*  var leftMenuItem = $('nav.navbar-fixed-left > ul.navbar-nav > li.menu-name');
-  leftMenuItem.hover(function() {
-    $(this).children('div.left-menu-main').fadeIn(500).removeClass('hidden');
-  }, function() {
-    $(this).children('div.left-menu-main').fadeOut(500).addClass('hidden');
-  })*/
+        $("div.gallery").click(function () {
+            $("#gallery-modal").modal();
+        });
 
-});
+
+        $("ul.mega-navbar li.mega-menu").hover(function () {
+                $(this).children("div.mega-menu-main").fadeIn(500).removeClass("hidden");
+        },
+            function () {
+                $(this).children("div.mega-menu-main").fadeOut(200).addClass("hidden");
+            });
+
+        //jquery for left menu
+        /*  var leftMenuItem = $('nav.navbar-fixed-left > ul.navbar-nav > li.menu-name');
+          leftMenuItem.hover(function() {
+            $(this).children('div.left-menu-main').fadeIn(500).removeClass('hidden');
+          }, function() {
+            $(this).children('div.left-menu-main').fadeOut(500).addClass('hidden');
+          })*/
+
+    });
+
+
+
+
+}());
